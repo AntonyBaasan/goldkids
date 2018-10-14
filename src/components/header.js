89fa8ce30d7bfc208e01619540b34ad3e630210a
Ankhbayar <antony.baasan@gmail.com>
2018-10-14 18:13:04 -0400

    add componentWillMount

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 export default class Header extends Component {
+    componentWillMount() {
+        console.log('Hello World Antony!');
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+            .then(response => {
+                console.log(response);
+            });
+    }
     render() {
         return (
             <View style={styles.viewStyle}>
