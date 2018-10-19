4b562ac289e33f70e131ec3829acd166d232b149
Ankhbayar <antony.baasan@gmail.com>
2018-10-18 23:32:59 -0400

    return back wrongly deleted file

---
+++ src/reducers/counterReducer.js
@@ -0,0 +1,12 @@
+import { INCREMENT, DECREMENT } from '../actions/actionTypes';
+
+export const counterReducer = (state = 10, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return state + action.payload;
+        case DECREMENT:
+            return state - action.payload;
+        default:
+            return state;
+    }
+};
