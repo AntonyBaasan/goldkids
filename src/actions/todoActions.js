7a1b0a9762d55efb20696eb984f948dbcd7ccb6a
Ankhbayar <antony.baasan@gmail.com>
2018-11-14 00:50:08 -0500

    refactor

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -6,21 +6,27 @@ export const updateTodo = ({ todoId, prop, value }) => ({
     payload: { todoId, prop, value },
 });
 
+const updateKidsCoins = (dispatch, state, todoId) => {
+    const { todos, kids, tasks } = state;
+    const todo = todos[todoId];
+
+    // only if done, than update kid's coins
+    if (todo.status === 'done') {
+        const kid = kids[todo.kidId];
+        const task = tasks[todo.refTaskId];
+        kid.coins += task.coins;
+        dispatch(updateKid({
+            id: todo.kidId,
+            kid
+        }));
+    }
+};
+
 /**
  * 1. update a todo object
  * 2. update weekly stats
  */
 export const updateTodoStatus = ({ todoId, newStatus }) => (dispatch, getState) => {
-        dispatch(updateTodo({ todoId, prop: 'status', value: newStatus }));
-
-        const { todos, kids, tasks } = getState();
-        const todo = todos[todoId];
-        const kidId = todo.kidId;
-        const kid = kids[kidId];
-        const task = tasks[todo.refTaskId];
-        kid.coins += task.coins;
-        dispatch(updateKid({
-            id: kidId,
-            kid
-        }));
-    };
+    dispatch(updateTodo({ todoId, prop: 'status', value: newStatus }));
+    updateKidsCoins(dispatch, getState(), todoId);
+};
