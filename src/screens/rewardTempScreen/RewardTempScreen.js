a2d8d649e6de84a532a7da227c2bf0eabd420353
Ankhbayar <antony.baasan@gmail.com>
2018-12-13 23:20:48 -0500

    remove unused code

--- src/screens/rewardTempScreen/RewardTempScreen.js
+++ src/screens/rewardTempScreen/RewardTempScreen.js
@@ -1,27 +1,15 @@
-import _ from 'lodash';
 import React, { Component } from 'react';
 import { View } from 'native-base';
 import { connect } from 'react-redux';
-import KidsList from '../todoScreen/KidsList';
-import RewardItem from './RewardItem';
 
 class RewardTempScreen extends Component {
     static navigationOptions = {
-        title: 'Todo',
+        title: 'Rewards',
     };
-
-    renderRewards() {
-        return _.map(this.props.rewards, (reward, rewardId) => (
-            <RewardItem key={rewardId} rewardId={rewardId} reward={reward} />
-        ));
-    }
 
     render() {
         return (
-            <View style={{ flex: 1 }}>
-                <KidsList />
-                {this.renderRewards()}
-            </View>
+            <View style={{ flex: 1 }} />
         );
     }
 }
